5b615b9b27de61764d917262a15f57fbfaef1938
mitchelln11 <mitchellnaleid@gmail.com>
2019-11-22 07:33:00 -0600

    linked superhero name in table to their own page

--- SuperheroCreator/SuperheroCreator/Models/Person.cs
+++ SuperheroCreator/SuperheroCreator/Models/Person.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
@@ -10,11 +11,23 @@ namespace SuperheroCreator.Models
     {
         [Key]
         public int Id { get; set; }
+
+        [DisplayName("Super Hero Name")]
         public string superHeroName { get; set; }
+
+        [DisplayName("Alias First Name")]
         public string firstName { get; set; }
+
+        [DisplayName("Alias Last Name")]
         public string lastName { get; set; }
+
+        [DisplayName("Primary Ability")]
         public string primaryAbility { get; set;  }
+
+        [DisplayName("Secondary Ability")]
         public string secondaryAbility { get; set; }
+
+        [DisplayName("Catch Phrase")]
         public string catchPhrase { get; set; }
     }
 }
